8d3ad2bf91c827c599d54d5af75abde2e64c96dc
yoon.homme <woosiks.io@gmail.com>
2025-12-23 16:37:32 +0900

    feat: Integrate Partytown for Google Tag Manager support, enhancing performance and user experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,17 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fredly.dev',
   integrations: [
     mdx({ remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] }),
     sitemap(),
+    partytown({
+      config: { forward: ['dataLayer.push'] },
+    }),
   ],
   trailingSlash: 'always',
 });
